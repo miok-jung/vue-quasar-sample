9de19d5482ae3200694736454954b216d2957643
mojung <mojung@ipactory.com>
2023-07-12 09:26:06 +0900

    feat: 타이포관련 예시문 추가

--- src/pages/main/Typography.vue
+++ src/pages/main/Typography.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>Typography</div>
+  <h1>HTML Heading1</h1>
+  <p class="text-h1">Quasar Heading1</p>
+  <h2>HTML Heading2</h2>
+  <p class="text-h2">Quasar Heading2</p>
+  <h3>HTML Heading3</h3>
+  <p class="text-h3">Quasar Heading3</p>
+  <h4>HTML Heading4</h4>
+  <p class="text-h4">Quasar Heading4</p>
+  <h5>HTML Heading5</h5>
+  <p class="text-h5">Quasar Heading5</p>
+  <h6>HTML Heading6</h6>
+  <p class="text-h6">Quasar Heading6</p>
+  <q-separator />
+  <p class="text-subtitle1">Subtitle 1</p>
+  <p class="text-subtitle2">Subtitle 2</p>
+  <p class="text-body1">
+    Body 1. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nisi sit, odit cum minima nihil!
+    Incidunt vero suscipit consequuntur. Dolor fugit dolores ad unde, soluta quaerat officia vero placeat tenetur.
+  </p>
+  <p class="text-body2">
+    Body 2. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi praesentium voluptates fugiat dolorum
+    illum tempora ullam magni enim, corporis ipsum. Optio vero tenetur nam quos ad quae reiciendis aliquid asperiores?
+  </p>
+  <p class="text-caption">Caption text</p>
+  <p class="text-overline">Overline</p>
+  <q-separator />
+  <p class="text-h2">Text Weight</p>
 </template>
 
 <script setup lang="ts"></script>
